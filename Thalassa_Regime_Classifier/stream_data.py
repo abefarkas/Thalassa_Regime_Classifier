265d4d555fa76364d819dd33948a7773faff999c
Fidel Perez <fi.macal@icloud.com>
2022-05-27 09:39:50 -0600

    stream data flow for mvp

--- Thalassa_Regime_Classifier/stream_data.py
+++ Thalassa_Regime_Classifier/stream_data.py
@@ -1,51 +1,48 @@
-import json
-import websocket
 import datetime
 import numpy as np
 import pandas as pd
-from datetime import datetime, timezone
-import time
+from datetime import datetime
+from websocket import create_connection
+import json
 
 class StreamingData():
     def __init__(self):
         self.depth = 5     
-        self.df_0 = None           
+        self.df_0 = None       
+        # self.model = joblib.load('/Users/fipm/code/abefarkas/Thalassa_Regime_Classifier/model.joblib')    
         self.symbol = 'BTCUSDT'        
         self.socket = 'wss://stream.binance.com:9443/ws/{}@depth{}'.format(self.symbol.lower(),self.depth)
-        
-    def handle_trades(self,json_message):    
-        df = pd.DataFrame.from_dict(self.my_json(json_message))
-        self.df_0 = pd.concat([self.df_0, df], axis=0)
-        # print(self.my_return())
-        
-    def my_return(self):
-        return self.preprocessing_streamed_data(self.df_0, rolling_window=2)
+        self.ws = None
+                
+    def get_stream_data(self, rolling_window=2):
+        '''clean the stream data'''
+        df = pd.DataFrame.from_dict(self.my_json(json.loads(self.ws.recv())))
+        self.df_0 = pd.concat([self.df_0, df], axis=0)        
+        # to keep in memory enough data to have 50 rows 
+        # after triggering preprocessing_streamed_data
+        self.df_0 = self.df_0.tail(200)
+        return self.preprocessing_streamed_data(self.df_0, rolling_window).reset_index(drop=True)
 
-    def on_message(self,wsapp,message):  
-        json_message = json.loads(message)
-        self.handle_trades(json_message)
-
-    def on_error(self,wsapp,error):
-        print(error)
-
-    def start(self):        
+    def start(self):       
+        '''start the connection with the server''' 
         self.df_0 = pd.DataFrame.from_dict(self.my_json_0(self.depth))
-        wsapp = websocket.WebSocketApp(self.socket, on_message=self.on_message, on_error=self.on_error)
-        wsapp.run_forever()
-        
-    def preprocessing_streamed_data(self,df_ob, rolling_window=2):
-        '''preprocessing of data for streamed data'''                
-        
+        self.ws = create_connection(self.socket)
+                
+    def preprocessing_streamed_data(self,df_ob, rolling_window):
+        '''preprocessing of data for streamed data'''                        
         # aggregating by seconds     
         df_agg = df_ob.groupby(pd.Grouper(key='primary_key', axis=0, freq='S')).mean()
         # applying rolling window of rolling_window lenght
         df_agg = df_agg.rolling(str(rolling_window)+'S').mean()
         # moving the index as a column
         df_agg.reset_index(inplace=True)
+        # keeping the last 50 rows (most recent information)        
+        df_agg = df_agg.dropna().tail(50)
 
         return df_agg
         
     def my_json(self, json_message):
+        '''transforming the data to a dict type'''
         size = len(np.array(json_message['bids'])[:,0])    
         return {
             **{'primary_key':[datetime.now()]},
@@ -54,7 +51,8 @@ class StreamingData():
             **{'ap'+str(key):[float(value)] for key,value in zip(np.arange(0,size)+1,np.array(json_message['asks'])[:,0])},
             **{'as'+str(key):[float(value)] for key,value in zip(np.arange(0,size)+1,np.array(json_message['asks'])[:,1])}}
     
-    def my_json_0(self, size):    
+    def my_json_0(self, size):
+        '''transforming the data to a dict type'''    
         return {
             **{'primary_key':[datetime.now()]},
             **{'bp'+str(key):[value] for key,value in zip(np.arange(0,size)+1,(np.arange(0,size)+1)*np.nan)},
@@ -62,7 +60,10 @@ class StreamingData():
             **{'ap'+str(key):[value] for key,value in zip(np.arange(0,size)+1,(np.arange(0,size)+1)*np.nan)},
             **{'as'+str(key):[value] for key,value in zip(np.arange(0,size)+1,(np.arange(0,size)+1)*np.nan)}}
 
+
 if __name__=='__main__':
     w = StreamingData()
     w.start()
-    w.my_return() # it is never triggered
+    while True:
+        print(w.get_stream_data(rolling_window=30))
+        
