45d7c8e0ad18cf3d35a234a66005b5d9d62f7fd5
Marcos Tellez <marcos.tlz.chr@gmail.com>
2022-05-30 12:12:18 -0400

    d

--- lib.py
+++ lib.py
@@ -1,114 +1,144 @@
 import numpy as np
+from sklearn.base import TransformerMixin, BaseEstimator
 import pandas as pd
 
-class FinancialFeatures():
-    def __init__(self, data):
-        self.data = data
+class FinancialFeatures(TransformerMixin, BaseEstimator):
+    def __init__(self):
+        pass
 
 
 
 
-    def WAP(self):
-        self.data['WAP'] = (self.data['bp1']*self.data['bs1']
-                +self.data['bp2']*self.data['bs2']
-                +self.data['ap1']*self.data['as1']
-                +self.data['ap2']*self.data['as2'])/(self.data['bs1']+
-                                            self.data['bs2']+
-                                            self.data['as1']+
-                                            self.data['as2'])
-        return self.data
+    def WAP(self, X):
+        X['WAP'] = (X['bp1']*X['bs1']
+                +X['bp2']*X['bs2']
+                +X['ap1']*X['as1']
+                +X['ap2']*X['as2'])/(X['bs1']+
+                                            X['bs2']+
+                                            X['as1']+
+                                            X['as2'])
+        return X
 
-    def build_df(self):
-        return self.data
+    def build_df(self, X):
+        return X
 
-    def spread(self):
-        self.data['spread'] = ((self.data['ap1']/self.data['bp1']) - 1)
-        return self.data
+    def spread(self, X):
+        X['spread'] = ((X['ap1']/X['bp1']) - 1)
+        return X
 
-    def log_price(self):
-        self.data['log_price'] = np.log(self.data['WAP'])
-        return self.data
+    def log_price(self, X):
+        X['log_price'] = np.log(X['WAP'])
+        return X
 
-    def log_returns(self):
-        self.data['log_returns'] = self.data.log_price.diff()
-        return self.data
+    def log_returns(self, X):
+        X['log_returns'] = X.log_price.diff()
+        return X
 
-    def volatility_df(self):
-        self.data['realized_volatility'] = np.std(self.data.log_returns)
-        return self.data
+    def volatility_df(self, X):
+        X['realized_volatility'] = np.std(X.log_returns)
+        return X
 
-    def volatility_next_period(self):
-        self.data['volatility_t+1'] = self.data['realized_volatility'].shift(-1)
-        return self.data
+    def volatility_next_period(self, X):
+        X['volatility_t+1'] = X['realized_volatility'].shift(-1)
+        return X
 
-    def dropping_columns(self):
-        self.data.drop(['Unnamed: 0'], axis = 1, inplace = True)
-        self.data.drop(['realized_volatility'], axis = 1, inplace = True)
-        return self.data
+    def dropping_columns(self, X):
+        X.drop(['Unnamed: 0'], axis = 1, inplace = True)
+        X.drop(['realized_volatility'], axis = 1, inplace = True)
+        return X
 
-    def first2_bid_depth(self):
-        self.data['first2_bid_depth'] = self.data[['bs1', 'bs2']].sum(axis=1)
-        return self.data
+    def first2_bid_depth(self, X):
+        X['first2_bid_depth'] = X[['bs1', 'bs2']].sum(axis=1)
+        return X
 
-    def first2_ask_depth(self):
-        self.data['first2_ask_depth'] = self.data[['as1', 'as2']].sum(axis=1)
-        return self.data
+    def first2_ask_depth(self, X):
+        X['first2_ask_depth'] = X[['as1', 'as2']].sum(axis=1)
+        return X
 
-    def full_bid_depth(self):
-        self.data['full_bid_depth'] = self.data[['bs1', 'bs2', 'bs3','bs4', 'bs5', 'bs6','bs7', 'bs8', 'bs9','bs10',
+    def full_bid_depth(self, X):
+        X['full_bid_depth'] = X[['bs1', 'bs2', 'bs3','bs4', 'bs5', 'bs6','bs7', 'bs8', 'bs9','bs10',
                             'bs11', 'bs12', 'bs13','bs14', 'bs15', 'bs16','bs17', 'bs18', 'bs19','bs20']].sum(axis=1)
-        return self.data
+        return X
 
-    def full_ask_depth(self):
-        self.data['full_ask_depth'] = self.data[['as1', 'as2', 'as3','as4', 'as5', 'as6','as7', 'as8', 'as9','as10',
+    def full_ask_depth(self, X):
+        X['full_ask_depth'] = X[['as1', 'as2', 'as3','as4', 'as5', 'as6','as7', 'as8', 'as9','as10',
                             'as11', 'as12', 'as13','as14', 'as15', 'as16','as17', 'as18', 'as19','as20']].sum(axis=1)
-        return self.data
+        return X
 
-    def BBAOFI(self):
-        self.data['BBAOFI'] = (self.data['bs1']-self.data['as1'])/(self.data['bs1']+self.data['as1'])
-        return self.data
+    def BBAOFI(self, X):
+        X['BBAOFI'] = (X['bs1']-X['as1'])/(X['bs1']+X['as1'])
+        return X
 
-    def first2_OFI(self):
-        self.data['First2OFI'] = ((self.data['bs1']+self.data['bs2']) - (self.data['as1']+self.data['as2']))/ ((self.data['bs1']+self.data['bs2']) + (self.data['as1']+self.data['as2']))
-        return self.data
+    def first2_OFI(self, X):
+        X['First2OFI'] = ((X['bs1']+X['bs2']) - (X['as1']+X['as2']))/ ((X['bs1']+X['bs2']) + (X['as1']+X['as2']))
+        return X
 
-    def FDOFI(self):
-        self.data['FDOFI'] = (self.data['full_bid_depth']-self.data['full_ask_depth'])/(self.data['full_bid_depth']+self.data['full_ask_depth'])
-        return self.data
+    def FDOFI(self, X):
+        X['FDOFI'] = (X['full_bid_depth']-X['full_ask_depth'])/(X['full_bid_depth']+X['full_ask_depth'])
+        return X
 
-    def set_index(self):
-        self.data.set_index('primary_key')
-        return self.data
+    def set_index(self, X):
+        X.set_index('primary_key')
+        return X
 
-    def WPA_trend(self):
-        self.data['WAP_trend5'] = self.data['WAP'].ewm(span=2).mean()
-        self.data['WAP_trend10'] = self.data['WAP'].ewm(span=5).mean()
-        self.data['WAP_trend20'] = self.data['WAP'].ewm(span=10).mean()
-        self.data['WAP_trend50'] = self.data['WAP'].ewm(span=20).mean()
-        self.data['WAP_trend100'] = self.data['WAP'].ewm(span=50).mean()
-        self.data['WAP_trend200'] = self.data['WAP'].ewm(span=100).mean()
-        self.data['WAP_trend1000'] = self.data['WAP'].ewm(span=200).mean()
-        return self.data
+    def WPA_trend(self, X):
+        X['WAP_trend5'] = X['WAP'].ewm(span=2).mean()
+        X['WAP_trend10'] = X['WAP'].ewm(span=5).mean()
+        X['WAP_trend20'] = X['WAP'].ewm(span=10).mean()
+        X['WAP_trend50'] = X['WAP'].ewm(span=20).mean()
+        X['WAP_trend100'] = X['WAP'].ewm(span=50).mean()
+        X['WAP_trend200'] = X['WAP'].ewm(span=100).mean()
+        X['WAP_trend1000'] = X['WAP'].ewm(span=200).mean()
+        return X
 
-    def first2_OFI_trend(self):
-        self.data['First2OFI_trend5'] = self.data['First2OFI'].ewm(span=2).mean()
-        self.data['First2OFI_trend10'] = self.data['First2OFI'].ewm(span=5).mean()
-        self.data['First2OFI_trend20'] = self.data['First2OFI'].ewm(span=10).mean()
-        self.data['First2OFI_trend50'] = self.data['First2OFI'].ewm(span=20).mean()
-        self.data['First2OFI_trend100'] = self.data['First2OFI'].ewm(span=50).mean()
-        self.data['First2OFI_trend200'] = self.data['First2OFI'].ewm(span=100).mean()
-        self.data['First2OFI_trend1000'] = self.data['First2OFI'].ewm(span=200).mean()
-        return self.data
+    def first2_OFI_trend(self, X):
+        X['First2OFI_trend5'] = X['First2OFI'].ewm(span=2).mean()
+        X['First2OFI_trend10'] = X['First2OFI'].ewm(span=5).mean()
+        X['First2OFI_trend20'] = X['First2OFI'].ewm(span=10).mean()
+        X['First2OFI_trend50'] = X['First2OFI'].ewm(span=20).mean()
+        X['First2OFI_trend100'] = X['First2OFI'].ewm(span=50).mean()
+        X['First2OFI_trend200'] = X['First2OFI'].ewm(span=100).mean()
+        X['First2OFI_trend1000'] = X['First2OFI'].ewm(span=200).mean()
+        return X
 
-    def FDOFI_trend(self):
-        self.data['FDOFI_trend5'] = self.data['FDOFI'].ewm(span=2).mean()
-        self.data['FDOFI_trend10'] = self.data['FDOFI'].ewm(span=5).mean()
-        self.data['FDOFI_trend20'] = self.data['FDOFI'].ewm(span=10).mean()
-        self.data['FDOFI_trend50'] = self.data['FDOFI'].ewm(span=20).mean()
-        self.data['FDOFI_trend100'] = self.data['FDOFI'].ewm(span=50).mean()
-        self.data['FDOFI_trend200'] = self.data['FDOFI'].ewm(span=100).mean()
-        self.data['FDOFI_trend1000'] = self.data['FDOFI'].ewm(span=200).mean()
-        return self.data
+    def FDOFI_trend(self, X):
+        X['FDOFI_trend5'] = X['FDOFI'].ewm(span=2).mean()
+        X['FDOFI_trend10'] = X['FDOFI'].ewm(span=5).mean()
+        X['FDOFI_trend20'] = X['FDOFI'].ewm(span=10).mean()
+        X['FDOFI_trend50'] = X['FDOFI'].ewm(span=20).mean()
+        X['FDOFI_trend100'] = X['FDOFI'].ewm(span=50).mean()
+        X['FDOFI_trend200'] = X['FDOFI'].ewm(span=100).mean()
+        X['FDOFI_trend1000'] = X['FDOFI'].ewm(span=200).mean()
+        return X
+
+    def fit(self, X, y=None):
+        # Store here what needs to be stored during .fit(X_train) as instance attributes.
+        # Return "self" to allow chaining .fit().transform()
+        return self
+
+
+    def transform(self, X, y=None):
+        # df = FinancialFeatures('/Users/marcostellez/code/abefarkas/Thalassa_Regime_Classifier/raw_data/data_set.csv')
+        self.WAP(X)
+        self.spread(X)
+        self.log_price(X)
+        self.log_returns(X)
+        self.volatility_df(X)
+        self.volatility_next_period(X)
+        self.dropping_columns(X)
+        self.first2_bid_depth(X)
+        self.first2_ask_depth(X)
+        self.full_bid_depth(X)
+        self.full_ask_depth(X)
+        self.BBAOFI(X)
+        self.first2_OFI(X)
+        self.FDOFI(X)
+        self.set_index(X)
+        self.WPA_trend(X)
+        self.first2_OFI_trend(X)
+        self.FDOFI_trend(X)
+        return self.build_df(X)
+
 
 
 def main():
